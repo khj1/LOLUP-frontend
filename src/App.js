f2effe4af5450ee0f4d7d7e414b31f3fb92035cf
qndrl1004 <qndrl1004@gmail.com>
2021-08-13 10:18:44 +0900

    Revert "로그인입니다."
    This reverts commit 06644e913dd5cdbc50f5540456c0c8633302124c.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React, { Component } from "react";
-import FadeIn from "react-fade-in";
-import "./App.css";
-import Login from "./component/Login";
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <FadeIn delay="700" transitionDuration="1000">
-          <Login />
-        </FadeIn>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
