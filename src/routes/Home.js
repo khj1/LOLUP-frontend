f8ed7f7126093cc9104f75a4c7a0fb17b43596c5
qndrl1004 <82863770+qndrl1004@users.noreply.github.com>
2021-08-23 17:03:52 +0900

    1차 수정도중 api이슈로 중간 푸쉬함 (#30)
    현 상황 사이드바 메인바 수정중
    듀오페이지 ui 설정중
    
    Co-authored-by: khj1 <74335027+khj1@users.noreply.github.com>

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,19 +1,17 @@
-import React, { useState, useCallback } from 'react';
-import Categories from '../components/Categories';
-import DuoList from '../components/DuoList';
+import React, { useState, useCallback } from "react";
+import Categories from "../components/Categories";
+import DuoList from "../components/DuoList";
 
-/* React Router 적용 X >> useState로 관리 */
+function Home() {
+  const [category, setCategory] = useState("all");
+  const onSelect = useCallback((category) => setCategory(category), []);
 
-const Home = () => {
-    const [ category, setCategory ] = useState('all');
-    const onSelect = useCallback(category => setCategory(category), []);
-
-    return (
-        <>
-            <Categories category={category} onSelect={onSelect} />
-            <DuoList category={category} />
-        </>
-    );
-};
+  return (
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <DuoList category={category} />
+    </>
+  );
+}
 
 export default Home;
