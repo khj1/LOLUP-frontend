817ee055b11340268681542ea33fd44767508fb4
parksangcheol-1119 <82863812+parksangcheol-1119@users.noreply.github.com>
2021-08-26 23:35:11 +0900

    탑 스크롤 추가와 헤더 css수정입니다. (#38)

--- src/components/DuoList.js
+++ src/components/DuoList.js
@@ -7,6 +7,7 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import Scroll from "./Scroll";
 
 export class DuoList extends Component {
   state = {
@@ -103,6 +104,7 @@ export class DuoList extends Component {
                 <StyledTableCell align="center">선호챔피언</StyledTableCell>
                 <StyledTableCell align="center">한줄소개</StyledTableCell>
                 <StyledTableCell align="center">등록날짜</StyledTableCell>
+                <Scroll/>
               </TableRow>
             </TableHead>
             <TableBody>
