8d326cc9b600cff4005aead4906c04a1eb661d9a
2jelee <2ttobokk@gmail.com>
2021-09-01 22:07:00 +0900

    feature#45 채팅창을 추가했습니다.

---
+++ src/components/buttons/Chatting.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import styled from 'styled-components'; 
+import { GlobalStyles } from './globalStyles';
+import Modal from './Modal';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+`;
+
+const Button = styled.button`
+  min-width: 100px;
+  padding: 16px 32px;
+  border-radius: 4px;
+  border: none;
+  background: none;
+  color: #fff;
+  font-size: 24px;
+  cursor: pointer; 
+`;
+
+function Chatting() {
+  const [showModal, setShowModal] = useState(false)
+  const openModal = () => {
+    setShowModal(prev=>!prev)
+  };
+ 
+  return (
+    <>
+      <Container>
+        <Button onClick={openModal}><img src={'/images/buttons/chat.png'} width="35px"/></Button> 
+        <Modal showModal={showModal} setShowModal={setShowModal} />
+        <GlobalStyles/>
+      </Container> 
+    </>
+  );
+}
+
+export default Chatting;
+
